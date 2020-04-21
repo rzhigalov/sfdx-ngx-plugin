d6654fa1a6ad782066b65ec8ed9774b4c27c8a52
Roman Zhigalov <larkie.wistful+github@gmail.com>
2020-04-21 13:04:37 +0300

    ✨ Initialize `ngx:build` command

---
+++ src/commands/ngx/build.ts
@@ -0,0 +1,42 @@
+import { SfdxCommand } from '@salesforce/command';
+import { Messages } from '@salesforce/core';
+import { AnyJson } from '@salesforce/ts-types';
+
+// Initialize Messages with the current plugin directory
+Messages.importMessagesDirectory(__dirname);
+
+// Load the specific messages for this file. Messages from @salesforce/command, @salesforce/core,
+// or any library that is using the messages framework can also be loaded this way.
+const messages = Messages.loadMessages('sfdx-ngx-plugin', 'ngx');
+
+export default class NgxBuild extends SfdxCommand {
+  public static description = messages.getMessage('buildCommandDescription');
+
+  public static examples = [
+    `$ sfdx ngx:build
+
+    ADD EXAMPLE OUTPUT
+  `,
+    `
+    ADD EXTENDED EXAMPLE DESCRIPTION
+  `
+  ];
+
+  protected static flagsConfig = {};
+
+  protected static requiresProject = true;
+
+  public async run(): Promise<AnyJson> {
+    this.ux.styledHeader('Building Angular project for SFDC');
+
+    // Return an object to be displayed with --json
+    const statusMessage = 'Angular project built and packed for SFDC deployment!';
+    this.ux.log(`\n${statusMessage}`);
+
+    // Return an object to be displayed with --json
+    return {
+      success: true,
+      message: statusMessage
+    };
+  }
+}
